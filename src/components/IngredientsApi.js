6230d523690cc3f381f2fd09c1a310e02e7fc452
Dana Manoli <dmanoli1@gmail.com>
2020-08-19 15:49:42 -0400

    grabbing ingredient results

---
+++ src/components/IngredientsApi.js
@@ -0,0 +1,26 @@
+import React, { useState, useEffect } from 'react';
+import IngredientList from './IngredientList';
+
+const IngredientsApi = () => {
+	const [ingredientOptions, getIngredientOptions] = useState([]);
+
+	useEffect(() => {
+		fetch(`https://the-cocktail-db.p.rapidapi.com/list.php?i=list`, {
+			method: 'GET',
+			headers: {
+				'x-rapidapi-host': 'the-cocktail-db.p.rapidapi.com',
+				'x-rapidapi-key': process.env.REACT_APP_COCKTAIL_DB_API_KEY,
+			},
+		})
+			.then((res) => res.json())
+			.then((ingredientData) => getIngredientOptions(ingredientData.drinks))
+			.catch((err) => console.log(err));
+	}, []);
+	return (
+		<>
+			<IngredientList options={ingredientOptions} />
+		</>
+	);
+};
+
+export default IngredientsApi;
